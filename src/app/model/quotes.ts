a848c86e5898213d7b1bb642def29acb1e187596
Michael Odongo <mikodongo@gmail.com>
2019-05-03 13:13:04 +0300

    editing and spacing of model quotes.ts

--- src/app/model/quotes.ts
+++ src/app/model/quotes.ts
@@ -2,11 +2,14 @@ export class Quotes {
   public showDescription: boolean;
 
   constructor(public id: number, public name: string, public myquote: string,
-    public author: string,
-    public postdate: Date, public upvote: number,
-    public downvote: number) {
+              public author: string,
+              public postdate: Date, public upvote: number,
+              public downvote: number) {
+// tslint:disable-next-line: semicolon
     this.showDescription = false
+// tslint:disable-next-line: semicolon
     this.upvote = 0
+// tslint:disable-next-line: semicolon
     this.downvote = 0
 
   }
