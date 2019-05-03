67cba520b92e2491322e95699999851db6665bc1
Michael Odongo <mikodongo@gmail.com>
2019-05-03 13:19:36 +0300

    updated directive and pipe .ts files

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -6,7 +6,21 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class DateCountPipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
-    return null;
-  }
+    const today: any = new Date(); // current date and time
+    // let todayWithNoTime:any = new Date(today.getFullYear(),today.getMonth(),today.getDate())
+    const dateDifference = Math.abs(value - today); // returns value in milliseconds
+    const secondsInAnHour = 3600; // 60 seconds*60 minutes in an hour
+
+    const dateDifferenceSeconds = dateDifference * 0.001; // converts to seconds
+
+    const dateCounter = Math.round(dateDifferenceSeconds / secondsInAnHour);
+    // var dateCounter = (dateCounter;
+
+
+    if (dateCounter >= 1) {
+      return dateCounter;
+    } else {
+      return 0;
+    }  }
 
 }
--- src/app/highlights.directive.ts
+++ src/app/highlights.directive.ts
@@ -1,10 +1,14 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appHighlights]'
 })
 export class HighlightsDirective {
 
-  constructor() { }
+  constructor(private elem: ElementRef) {
+    this.elem.nativeElement.style.boxShadow = '0 0 5px rgba(81, 203, 238, 1)';
+    this.elem.nativeElement.style.background = 'rgba(81, 203, 238, 0.13)';
+    this.elem.nativeElement.style.padding = '.2rem';
+  }
 
 }
